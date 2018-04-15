f3bacff2bc8b553dfbd02cde01b433322a351eef
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2018-04-15 12:01:33 -0500

    Conexion a red WiFi

--- IoTViewEsp32.ino
+++ IoTViewEsp32.ino
@@ -13,6 +13,7 @@
 
 #define TPrueba 50
 
+char host[]="iotview.herokuapp.com";
 
 int status = WL_IDLE_STATUS;
 
@@ -25,42 +26,21 @@ void setup() {
   Serial.println("IoTView en ESP32");
   Serial.println("Desarrollado por:");
   Serial.println("Ing. César Augusto Álvarez Gaspar");
-  WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(100);
-
-    Serial.println("Setup done");
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  Serial.println("scan start");
-
-    // WiFi.scanNetworks will return the number of networks found
-    int n = WiFi.scanNetworks();
-    Serial.println("scan done");
-    if (n == 0) {
-        Serial.println("no networks found");
-    } else {
-        Serial.print(n);
-        Serial.println(" networks found");
-        for (int i = 0; i < n; ++i) {
-            // Print SSID and RSSI for each network found
-            Serial.print(i + 1);
-            Serial.print(": ");
-            Serial.print(WiFi.SSID(i));
-            Serial.print(" (");
-            Serial.print(WiFi.RSSI(i));
-            Serial.print(")");
-            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-            delay(10);
-        }
-    }
-    Serial.println("");
-
-    // Wait a bit before scanning again
-    delay(5000);
-  /*
-    for(int i=0;i<N_SENO;i++)
+  
+  
+  /*  for(int i=0;i<N_SENO;i++)
     {
       Serial.println(PruebaSin[i]);
       delay(TPrueba); 
